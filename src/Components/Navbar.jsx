d3134a034757f32880635ececd1df5a6ad09f5bd
TamannaTahsin1 <tamanntahsin648@gmail.com>
2024-01-11 11:25:48 +0600

    project setup

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>Model</a></li>
+      <li><a>Philosophy</a></li>
+      <li><a>Project Partner</a></li>
+      <li><a>Join us</a></li>
+      <li><a>Team</a></li>
+      <li><a>Supporters</a></li>
+      <li><a>Link</a></li>
+
+    </ul>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
