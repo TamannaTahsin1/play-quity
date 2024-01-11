12333b29441454f858cd0c7f3c42dfcce985fefa
TamannaTahsin1 <tamanntahsin648@gmail.com>
2024-01-11 11:30:12 +0600

    router setup

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,76 @@
-
+import { IoMdSearch } from "react-icons/io";
 
 const Navbar = () => {
-    return (
-        <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+  return (
+    <div className="navbar bg-base-100">
+    <div className="navbar-start">
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        </div>
+        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Model</a>
+          </li>
+          <li>
+            <a>Philosophy</a>
+          </li>
+          <li>
+            <a>Project Partner</a>
+          </li>
+          <li>
+            <a>Join us</a>
+          </li>
+          <li>
+            <a>Team</a>
+          </li>
+          <li>
+            <a>Supporters</a>
+          </li>
+          <li className="text-center text-2xl">
+            <a>
+              <IoMdSearch />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div className="navbar-end hidden lg:flex">
+      <ul className="menu menu-horizontal px-1">
+      <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Model</a>
+          </li>
+          <li>
+            <a>Philosophy</a>
+          </li>
+          <li>
+            <a>Project Partner</a>
+          </li>
+          <li>
+            <a>Join us</a>
+          </li>
+          <li>
+            <a>Team</a>
+          </li>
+          <li>
+            <a>Supporters</a>
+          </li>
+          <li className="text-center text-2xl">
+            <a>
+              <IoMdSearch />
+            </a>
+          </li>
+      </ul>
+    </div>
   </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-      <li><a>Home</a></li>
-      <li><a>Model</a></li>
-      <li><a>Philosophy</a></li>
-      <li><a>Project Partner</a></li>
-      <li><a>Join us</a></li>
-      <li><a>Team</a></li>
-      <li><a>Supporters</a></li>
-      <li><a>Link</a></li>
-
-    </ul>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Navbar;
