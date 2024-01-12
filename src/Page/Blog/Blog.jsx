0e8bf47909dec1a61d6977507164458d87abf3e8
TamannaTahsin1 <tamanntahsin648@gmail.com>
2024-01-12 21:04:59 +0600

    blog written start

--- src/Page/Blog/Blog.jsx
+++ src/Page/Blog/Blog.jsx
@@ -1,65 +1,62 @@
 import img1 from "../../assets/img/img1.png";
-
+import "./Blog.css";
 const Blog = () => {
   return (
     <div>
       <h1 className="text-center bg-purple-700 py-10 text-white text-5xl  md:mt-3">
         Our Blog
       </h1>
-        {/* first point */}
+      {/* first point */}
       <div className="max-w-[1500px] mx-auto xl:px-20 md:px-10 sm:px-2 px-4 mt-10">
         <div className="grid grid-cols-1 lg:flex lg:justify-center lg:items-center lg:gap-10 my-10">
-          <h1 className="text-center w-16 text-4xl text-white font-bold p-2 px-5 shadow-purple-600 rounded-full bg-orange-500">
-            1
-          </h1>
+          <h1 className="rounded">1</h1>
           <div>
             <p className="text-2xl text-purple-600 mb-9">
-              Stage One - Incubator: <br />
-              Training girls to be spirited athletes
+              Stage One - Home: <br />
+              Project Details
             </p>
-            <p className="text-sm">
-              Playquity will provide a comprehensive ultimate frisbee training
-              program for girls (only) <br /> that will enable them to develop
-              not only athletic skills but also confidence, empathy, <br />{" "}
-              communication and inter-personal skills that are critical to their
-              development. Special <br /> sessions for the mothers of the girls
-              will be conducted to earn their trust and to <br /> strengthen the
-              effects of our programming. At the end of this stage, Playquity
-              will help <br /> the girls identify coaches, managers, and player
-              positions.
+            <p className="text-base">
+              In Playquity website, their is a progressive <br /> organization
+              dedicated to building equity through play <br /> by challenging
+              traditional gender roles. Their ultimate <br /> frisbee coaching
+              program empowers girls to become <br /> spirited athletes,
+              fostering both sports and leadership <br /> skills. The initiative
+              goes beyond the field, aiming to instill <br />
+              confidence and independence in girls, preparing them to take on <br />
+              coaching  roles in their communities. Also there are a model
+              section where they <br /> give  a detail view of their learning
+              structure.
             </p>
           </div>
           <img src={img1} className="md:w-[700px] md:h-72 mt-5" alt="" />
         </div>
         {/* second point */}
         <div className="grid grid-cols-1 lg:flex lg:justify-center lg:items-center lg:gap-10 my-10">
-         <div className="text-center">
-         <h1 className=" text-center w-16 text-4xl text-white font-bold p-2 px-5 shadow-purple-600 rounded-full bg-orange-500">
-            1
-          </h1>
-         </div>
+          <div className="text-center">
+            <h1 className=" rounded">2</h1>
+          </div>
           <div>
             <p className="text-2xl text-purple-600 mb-9">
-              Stage One - Incubator: Training girls to be spirited athletes
+              Stage One - Functionality: <br /> Routes, buttons and others
             </p>
             <p>
-              Playquity will provide a comprehensive ultimate frisbee training
-              program for girls (only) <br /> that will enable them to develop
-              not only athletic skills but also confidence, empathy, <br />{" "}
-              communication and inter-personal skills that are critical to their
-              development. Special <br /> sessions for the mothers of the girls
-              will be conducted to earn their trust and to <br /> strengthen the
-              effects of our programming. At the end of this stage, Playquity
-              will help <br /> the girls identify coaches, managers, and player
-              positions.
+              All buttons on the website have real functions. <br />
+              Each button redirects users to different links, <br /> ensuring a
+              practical and efficient user experience. <br /> Sections on the
+              website align with route names, making navigation intuitive. Users
+              can expect that each section is directly related to the indicated
+              route, streamlining the overall structure. In addition, I link the
+              join us section where their is form so that user can use the form
+              if they want
             </p>
           </div>
           <img src={img1} className="md:w-[800px] md:h-72" alt="" />
         </div>
+        {/* third point */}
         <div className="grid grid-cols-1 lg:flex lg:justify-center lg:items-center lg:gap-10 my-10">
-          <h1 className=" w-16 text-center text-4xl text-white font-bold p-2 px-5 shadow-purple-600 rounded-full bg-orange-500">
-            1
-          </h1>
+          <div className="text-center">
+            <h1 className=" rounded">3</h1>
+          </div>
           <div>
             <p className="text-2xl text-purple-600 mb-9">
               Stage One - Incubator: Training girls to be spirited athletes
@@ -67,7 +64,7 @@ const Blog = () => {
             <p>
               Playquity will provide a comprehensive ultimate frisbee training
               program for girls (only) <br /> that will enable them to develop
-              not only athletic skills but also confidence, empathy, <br />{" "}
+              not only athletic skills but also confidence, empathy, <br />
               communication and inter-personal skills that are critical to their
               development. Special <br /> sessions for the mothers of the girls
               will be conducted to earn their trust and to <br /> strengthen the
